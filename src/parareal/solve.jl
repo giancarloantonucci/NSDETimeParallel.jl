7db7f12c3de8bf6ed566205361f4254ee9aa44b3
Giancarlo Antonucci <giancarlo.antonucci@icloud.com>
2021-07-19 16:07:26 +0100

    ...

--- src/parareal/solve.jl
+++ src/parareal/solve.jl
@@ -13,26 +13,44 @@ function solve!(solution::TimeParallelSolution, problem, solver::Parareal)
     @everywhere problem = $problem
     for k = 1:K
         # fine run (parallelisable)
-        if nprocs() == 1 || mode == "SERIAL"
+        if mode == "SERIAL" || nprocs() == 1
             for n = k:P
                 chunk = ℱ(problem, U[n], T[n], T[n+1])
                 solution[k][n] = chunk
                 F[n+1] = chunk.u[end]
             end
-        elseif nprocs() > 1 && P == nworkers()
-            ws = (k:P)
-            @everywhere ws begin
-                n = myid()
-                U = $U
-                T = $T
-                Un = U[n]
-                Tn = T[n]
-                Tp = T[n+1]
-                chunk = ℱ(problem, Un, Tn, Tp)
+        elseif (mode == "PARALLEL" || mode == "PARALLEL2") && nprocs() > 1
+            # ws = (k:P)
+            # @everywhere ws begin
+            #     n = myid()
+            #     U = $U
+            #     T = $T
+            #     Un = U[n]
+            #     Tn = T[n]
+            #     Tp = T[n+1]
+            #     chunk = ℱ(problem, Un, Tn, Tp)
+            # end
+            # for n in ws
+            #     chunk = @fetchfrom n Main.chunk
+            #     solution[k][n] = chunk
+            #     F[n+1] = chunk.u[end]
+            # end
+            if mode == "PARALLEL2"
+                @everywhere k:P begin
+                    n = myid()
+                    U = $U
+                    T = $T
+                end
             end
-            for n in ws
-                chunk = @fetchfrom n Main.chunk
-                solution[k][n] = chunk
+            @sync begin
+                for n = k:P
+                    @async begin
+                        solution[k][n] = remotecall_fetch(ℱ, n, problem, U[n], T[n], T[n+1])
+                    end
+                end
+            end
+            for n = k:P
+                chunk = solution[k][n]
                 F[n+1] = chunk.u[end]
             end
         end
