5ac00b1f97c5f67b7be7d53efbf1ca6cf0d962b0
Giancarlo Antonucci <giancarlo.antonucci@icloud.com>
2021-07-20 17:05:23 +0100

    Create `parareal.jl`

--- src/parareal.jl
+++ src/parareal.jl
@@ -40,3 +40,47 @@ function Parareal(finesolver::InitialValueSolver, coarsolver::InitialValueSolver
     end
     return Parareal(objective, mode, ℱ, 𝒢, P, K)
 end
+
+function solve!(solution::TimeParallelSolution, problem, solver::Parareal)
+    @↓ iterates, φ, U, T = solution
+    @↓ mode, ℱ, 𝒢, P, K = solver
+    @↓ 𝜑, ϵ = solver.objective
+    # coarse guess
+    G = similar(U); G[1] = U[1]
+    for n = 1:P
+        chunk = 𝒢(problem, U[n], T[n], T[n+1])
+        G[n+1] = chunk.u[end]
+    end
+    # main loop
+    F = similar(U); F[1] = U[1]
+    getF(args...) = ℱ(args...).u[end]
+    for k = 1:K
+        # fine run (parallelisable)
+        if mode == "SERIAL" || nprocs() == 1
+            for n = k:P
+                chunk = ℱ(problem, U[n], T[n], T[n+1])
+                solution[k][n] = chunk
+                F[n+1] = chunk.u[end]
+            end
+        elseif mode == "PARALLEL" && nprocs() > 1
+            @sync for n = k:P
+                @async F[n+1] = remotecall_fetch(getF, n, problem, U[n], T[n], T[n+1])
+            end
+        end
+        # check convergence
+        φ[k] = 𝜑(U, F, T)
+        if φ[k] ≤ ϵ
+            resize!(iterates, k)
+            resize!(φ, k)
+            break
+        end
+        # update (serial)
+        for n = k:P
+            chunk = 𝒢(problem, U[n], T[n], T[n+1])
+            U[n+1] = chunk.u[end] + F[n+1] - G[n+1]
+            G[n+1] = chunk.u[end]
+        end
+        @↑ solution = U, T
+    end
+    return solution
+end
