029371491980f357466c3577f5144d76ecc1bf0f
Giancarlo Antonino Antonucci <giancarlo.antonucci@icloud.com>
2022-11-29 18:45:08 +0000

    Delete abstract.jl

--- src/abstract.jl
+++
@@ -1,8 +0,0 @@
-abstract type AbstractTimeParallelSolver <: AbstractInitialValueSolver end
-abstract type AbstractTimeParallelSolution <: AbstractInitialValueSolution end
-abstract type AbstractTimeParallelIterate <: AbstractInitialValueSolution end
-abstract type AbstractTimeParallelCache <: AbstractInitialValueCache end
-abstract type AbstractTimeParallelParameters <: AbstractInitialValueParameters end
-
-abstract type AbstractTolerance <: AbstractTimeParallelParameters end
-abstract type AbstractWeights <: AbstractTimeParallelParameters end
