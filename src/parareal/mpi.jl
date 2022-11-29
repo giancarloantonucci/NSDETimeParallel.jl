e728c5d0767e39f0398f75a3175085175f444244
Giancarlo Antonino Antonucci <giancarlo.antonucci@icloud.com>
2022-11-29 18:44:56 +0000

    Delete src/parareal directory

--- src/parareal/mpi.jl
+++
@@ -1,4 +0,0 @@
-"MPI implementation of Parareal."
-function parareal_mpi!(cache::PararealCache, solution::PararealSolution, problem::AbstractInitialValueProblem, parareal::Parareal)
-    return error("Missing implementation.")
-end
