85c492b8f081431825901cfd3e3d3f05d61d76fb
Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>
2023-11-22 07:39:36 +0000

    Remove commented code

--- src/parareal/constructor.jl
+++ src/parareal/constructor.jl
@@ -29,11 +29,6 @@ struct Parareal{finesolver_T<:AbstractInitialValueSolver, coarsolver_T<:Abstract
     parameters::parameters_T
     tolerance::tolerance_T
     saveiterates::saveiterates_T
-    # function Parareal(finesolver::finesolver_T, coarsesolver::coarsolver_T, parameters::parameters_T, tolerance::tolerance_T, saveiterates::saveiterates_T) where {finesolver_T<:AbstractInitialValueSolver, coarsolver_T<:AbstractInitialValueSolver, parameters_T<:AbstractPararealParameters, tolerance_T<:AbstractTolerance, saveiterates_T<:Bool}
-    #     finesolver2 = deepcopy(finesolver)
-    #     coarsolver2 = deepcopy(coarsesolver)
-    #     return new{finesolver_T, coarsolver_T, parameters_T, tolerance_T, saveiterates_T}(finesolver2, coarsolver2, parameters, tolerance, saveiterates)
-    # end
 end
 
 Parareal(finesolver::AbstractInitialValueSolver, coarsesolver::AbstractInitialValueSolver; parameters::AbstractPararealParameters = PararealParameters(), tolerance::AbstractTolerance = Tolerance(), saveiterates::Bool = false) = Parareal(finesolver, coarsesolver, parameters, tolerance, saveiterates)
