5eec6d96907ef3d41dbee16575b185664ad24a2c
Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>
2024-03-03 23:45:11 +0000

    Add standard check

--- src/parareal/distributed.jl
+++ src/parareal/distributed.jl
@@ -7,6 +7,8 @@ function parareal_distributed!(cache::PararealCache, solution::PararealSolution,
     @↓ N, K = parareal.parameters
     @↓ weights, ψ, ϵ = parareal.tolerance
 
+    U_ = copy(U) # used by standard conv check
+
     # coarse run (serial)
     for n = 1:N
         if skips[n] # `skips[1] == true` always
@@ -53,7 +55,7 @@ function parareal_distributed!(cache::PararealCache, solution::PararealSolution,
 
         # check convergence
         update!(weights, U, F)
-        errors[k] = ψ(cache, solution, k, weights)
+        errors[k] = ψ(cache, solution, k, weights, U_)
         if errors[k] ≤ ϵ
             resize!(errors, k) # self-updates in solution
             if saveiterates
@@ -62,6 +64,8 @@ function parareal_distributed!(cache::PararealCache, solution::PararealSolution,
 
             break
         end
+
+        U_ .= U
 
         # correction step (serial)
         for n = k:N-1
