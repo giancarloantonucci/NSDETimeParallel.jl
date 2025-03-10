9ef054e8811a154be310b8febc56f2775d25f6fe
Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>
2025-03-10 20:09:57 +0000

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,6 @@
 using NSDETimeParallel
 using Test
 
-using Pkg
-for (uuid, pkg) in Pkg.dependencies()
-    if pkg.name == "NSDEBase"
-        @info "Testing NSDERungeKutta with NSDEBase version: $(pkg.version) from $(pkg.source)"
-    end
-end
-
 @testset "ErrorControl" begin
     # Write your tests here.
 end
